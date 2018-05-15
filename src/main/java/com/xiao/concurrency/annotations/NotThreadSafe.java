3596d35bcadcbe7bb1d3bd2cabd6c75117ecb153
jiangxinquqi <xiaoxiao65535@163.com>
2018-05-15 16:43:02 +0800

    线程不安全示例

--- src/main/java/com/xiao/concurrency/annotations/NotThreadSafe.java
+++ src/main/java/com/xiao/concurrency/annotations/NotThreadSafe.java
@@ -6,9 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 标识类是【线程不安全】的类或者是写法
+ * 【线程不安全】的类或者是写法
  */
-@Target(ElementType.TYPE) // 注解作用的目标
+@Target({ElementType.TYPE, ElementType.METHOD}) // 注解作用的目标
 @Retention(RetentionPolicy.SOURCE) // 在编译的时候忽略
 public @interface NotThreadSafe {
 
